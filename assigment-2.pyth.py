fd2f4d0244c40e803f7219b98ffc260ff8c9ddb1
vadheraryan <vadheraryan649@gmail.com>
2026-01-21 08:44:25 +0530

    Add files via upload

---
+++ assigment-2.pyth.py
@@ -0,0 +1,55 @@
+# Print numbers from 1 to 5
+for i in range(1, 6):
+    print(i)
+
+# Find length of a string
+my_string = "Hello, Python!"
+length = len(my_string)
+print(length)
+
+# Find maximum of two numbers
+a = 10
+b = 20
+maximum = max(a, b)
+print("Maximum is:", maximum)
+
+# Simple Interest calculation
+P = 1000
+R = 5
+T = 2
+SI = (P * R * T) / 100
+print("Simple Interest is:", SI)
+
+# Print welcome
+print("welcom")
+
+# First character of string
+my_string = "Python"
+first_char = my_string[0]
+print("The first character is:", first_char)
+
+# Last character of string
+last_char = my_string[-1]
+print("The last character is:", last_char)
+
+# Check positive, negative or zero
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+# Sum of three numbers
+a = 10
+b = 20
+c = 30
+sum_of_numbers = a + b + c
+print("The sum is:", sum_of_numbers)
+
+# Sum of two user input numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_of_numbers = num1 + num2
+print("The sum of the three numbers is:", sum_of_numbers)
